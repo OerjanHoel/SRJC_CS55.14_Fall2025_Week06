b2d2eb059567ce05eeb6644b986328b4cd8b5b19
Oerjan Hoel <orjan.hoel@gmail.com>
2025-09-29 17:18:25 -0700

    FInal Submission - Commencts Added

--- nextjs-blog/lib/firebase.js
+++ nextjs-blog/lib/firebase.js
@@ -1,13 +1,15 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-// import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from "firebase/app"; // Imported from firebase module
+import { getFirestore } from "firebase/firestore"; // Imported from firebase module
+// import { getAnalytics } from "firebase/analytics"; // No in use
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+// Secrets saved in other location to prevent push to git repo
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_APIKEY,
   authDomain: process.env.FIREBASE_AUTHDOMAIN,
@@ -21,7 +23,8 @@ const firebaseConfig = {
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
 const db = getFirestore(firebaseApp);
-
+// Export the db so other files
 export { db };
 
+// Not using analytics commented out
 // const analytics = getAnalytics(app);
--- nextjs-blog/lib/posts-firebase.js
+++ nextjs-blog/lib/posts-firebase.js
@@ -1,43 +1,49 @@
-// Import the firebase app instance
 import { db } from './firebase'; // Import database from firebase.js
 import { collection, getDocs, query, where, documentId } from 'firebase/firestore'; // Imported from Firebase node package
 
+// Export gets the data we want from our database at Firebase cretes array
 export async function getSortedPostsData() {
-    const myCollectionRef = collection(db, "posts");
-    const querySnapshot = await getDocs(myCollectionRef);
-    const jsonObj = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-    // Sort our object based on tile
+    const myCollectionRef = collection(db, "posts"); // Gets data from dabase(collection) named posts
+    const querySnapshot = await getDocs(myCollectionRef); // Waiting for the Docs to be loaded then add data to variable querySnapshot
+    const jsonObj = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() })); // Map all the objects that we have in our database
+    // Sort our object based on title
     jsonObj.sort(function (a, b) {
-            return a.title.localeCompare(b.title);
-        });
-        // Give us the output from the mapping of the array
-        return jsonObj.map(item => {
-            return {
-                id: item.id.toString(),
-                title: item.title,
-                date: item.date
-            }
-        });
-
-}
-
-export async function getAllPostIds() {
-    const myCollectionRef = collection(db, "posts");
-    const querySnapshot = await getDocs(myCollectionRef);
-    const jsonObj = querySnapshot.docs.map(doc => ({ id: doc.id }));
-    console.log(jsonObj);
+        return a.title.localeCompare(b.title);
+    });
+    // Give us the output from the mapping of the array
     return jsonObj.map(item => {
-    return {
-         params: {
-            id: item.id.toString()
-         }
-      }
+        return {
+            id: item.id.toString(),
+            title: item.title,
+            date: item.date
+        }
     });
 
 }
 
+// Export creates jsonObj and maps the id to string
+export async function getAllPostIds() {
+    const myCollectionRef = collection(db, "posts"); // Gets data from dabase(collection) named posts
+    const querySnapshot = await getDocs(myCollectionRef); // Waiting for the Docs to be loaded then add data to variable querySnapshot
+    const jsonObj = querySnapshot.docs.map(doc => ({ id: doc.id })); // Map all the objects that we have in our database
+    console.log(jsonObj);
+    // array of the database random created id's 
+    return jsonObj.map(item => {
+        return {
+            params: {
+                id: item.id.toString()
+            }
+        }
+    });
+
+}
+
+// Export the data we need to build the posts page
+// 'id' is the random id we created in Firebase when creating the posts
+// This turns into the new unique URL for each post
 export async function getPostData(id) {
-    const myCollectionRef = collection(db, "posts");
+    const myCollectionRef = collection(db, "posts"); // Gets data from dabase(collection) named posts
+    // Query that uses Firebase node sort the data we have
     const searchQuery = query(
         myCollectionRef,
         where(
@@ -46,9 +52,10 @@ export async function getPostData(id) {
             id
         )
     );
-    const querySnapshot = await getDocs(searchQuery);
-    const jsonObj = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+    const querySnapshot = await getDocs(searchQuery); // Waiting for the Docs to be loaded then add data to variable querySnapshot
+    const jsonObj = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() })); // Map all the objects that we have in our database
 
+    // if statement to check that resturns our objects
     if (jsonObj.length === 0) {
         return {
             id: id,
